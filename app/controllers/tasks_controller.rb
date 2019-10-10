967613bf39b337b5b6fe2d0ba331b02c8b3abfee
percy1998 <percy3227@yahoo.co.jp>
2019-10-10 08:51:09 +0000

    edit post & for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
-before_action :require_user_logged_in, only: [:index, :show]
+before_action :require_user_logged_in
+before_action :correct_user, only:[:edit, :destroy, :show]
 
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
   
   def show 
@@ -10,17 +14,18 @@ before_action :require_user_logged_in, only: [:index, :show]
   end
   
   def new 
-    @task = Task.new
+    @task = current_user.tasks.build
   end 
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
        flash[:success] = 'Taskが正常に投稿されました'
        redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Taskが投稿されませんでした'
-      render :new
+      render 'index'
     end
   end
     
@@ -42,7 +47,6 @@ before_action :require_user_logged_in, only: [:index, :show]
   def destroy
     @task  = Task.find(params[:id])
     @task.destroy
-    
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_url
   end
@@ -51,5 +55,11 @@ before_action :require_user_logged_in, only: [:index, :show]
     
   def task_params
       params.require(:task).permit(:content, :status)
+  end
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+    redirect_to root_url
+    end
   end
 end
