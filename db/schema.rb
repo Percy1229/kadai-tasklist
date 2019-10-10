d21dfce7dd3617547ba9ec1654e9698e4fdb7033
percy1998 <percy3227@yahoo.co.jp>
2019-10-10 05:34:53 +0000

    user signup & modification of error_messages and gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_081448) do
+ActiveRecord::Schema.define(version: 2019_10_09_082957) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
